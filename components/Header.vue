484b95c66cea3a48e9b5e74eb15d3a5c6771a96c
nznmvs <nznmvs@gmail.com>
2021-03-23 11:04:49 +0200

    header mobile complete

--- components/Header.vue
+++ components/Header.vue
@@ -18,16 +18,30 @@
 				/>
 			</div>
 		</div>
+		<Slide
+			class="burgerMenu"
+			:closeOnNavigation="true"
+			noOverlay
+		>
+			<a href="#aboutProducts"><span>{{ getHeaderText.aboutProduct }}</span></a>
+			<a href="#advantages"><span>{{ getHeaderText.advantages }}</span></a>
+			<a href="#review"><span>{{ getHeaderText.review }}</span></a>
+			<a href="#cooperation"><span>{{ getHeaderText.cooperation }}</span></a>
+			<a href="#contacts"><span>{{ getHeaderText.contact }}</span></a>
+		</Slide>
 	</div>
 </template>
 
 <script>
 	import LanguageChanger from '@/components/LanguageChanger';
 	import {mapActions, mapGetters, mapState} from 'vuex';
+	import { Slide } from 'vue-burger-menu'
 
 	export default {
 		name: "Header",
-		components: {LanguageChanger},
+		components: {
+			Slide,
+		},
 		methods: {
 			...mapActions(['setTextConfig']),
 		},
@@ -37,8 +51,9 @@
 	}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "styles/components/imports/variables";
+@import 'styles/components/imports/_breakpoints.scss';
 
 	.header {
 		display: flex;
@@ -47,12 +62,76 @@
 		height: 8.125em;
 		max-width: 68.75em;
 		margin:0 auto;
+		padding: 0 1em;
+
+		@include media-breakpoint-down($desktop-breakpoint) {
+			justify-content: center;
+		}
 
 		.logo {
 			width: 7em;
 			height: 3.75em;
 			background: url('~/static/icon/logo.svg') center no-repeat;
 			cursor: pointer;
+		}
+
+		.burgerMenu {
+			display: none;
+
+			.bm-burger-button {
+				top: 3.2em;
+				left: 1em;
+			}
+			.bm-burger-bars {
+				background-color: $purple;
+			}
+			.cross-style {
+				right: 1em;
+				top: 1.5em;
+
+				&:before {
+					content: '';
+					position: absolute;
+					left: -0.8em;
+					top: -0.5em;
+					width: 2em;
+					height: 2em;
+				}
+			}
+			.bm-cross {
+				background: $white;
+			}
+			.bm-menu {
+				background-color: $purple; /* Black*/
+			}
+
+			.bm-item-list {
+				color: $white;
+				margin-left: 10%;
+				font-family: $loadedFamilyMedium;
+				font-size: 1.25em;
+				line-height: 2.75em;
+				text-decoration: none;
+				text-transform: uppercase;
+
+				&:hover {
+					text-decoration: underline;
+				}
+			}
+			.bm-item-list > * {
+				display: flex;
+				text-decoration: none;
+				padding: 0.7em;
+			}
+			.bm-item-list > * > span {
+				margin-left: 10px;
+				font-weight: 700;
+				color: white;
+			}
+
+			@include media-breakpoint-down($desktop-breakpoint) {
+				display: block;
+			}
 		}
 
 		.menu {
@@ -67,6 +146,12 @@
 				font-family: $loadedFamilyMedium;
 				font-size: 1.25em;
 				line-height: 2.75em;
+
+				@include media-breakpoint-down($desktop-breakpoint) {
+					display: none;
+					position: absolute;
+					right: 1em;
+				}
 
 				&>li {
 					&>a {
@@ -86,6 +171,11 @@
 
 			.additional {
 				margin-left: 3em;
+
+				@include media-breakpoint-down($desktop-breakpoint) {
+					position: absolute;
+					right: 1em;
+				}
 			}
 		}
 	}
